087c05806af8a5b67c900e00c321ae76f9db7e42
PostgreSQL administrator <manuparra@gmail.com>
2021-09-14 14:20:20 +0000

    Updated first test in working status

--- src/tests/bucardo-benchmarking.py
+++ src/tests/bucardo-benchmarking.py
@@ -2,6 +2,7 @@ import psycopg2
 import datetime
 import time
 import unittest
+import logging,sys
 from database_replicator import DatabaseReplicator
 
 
@@ -15,23 +16,28 @@ class TestReplicationBenchmarking(unittest.TestCase):
         """
         self.dbr = DatabaseReplicator()
         self.dbr.add_master(
-            host="localhost", 
+            host="192.168.100.213", 
             database="sofiadb",
             user="postgres",
-            password=""
+            password="",
+            port=18020
         )
         self.dbr.add_bucardo(
-            host="localhost",
+            host="192.168.100.213",
             database="bucardo",
             user="postgres",
             password="",
-            port=""
+            port=18020
         )
 
     def test_benchmark_100_insert(self):
         """
         """
-        id_sync = 100
+
+        logging.basicConfig( stream=sys.stderr )
+        logging.getLogger( "SomeTest.testSomething" ).setLevel( logging.DEBUG )
+
+        id_sync = 102
         duration = 0.0
         timeout = 1200.0
 
@@ -45,14 +51,13 @@ class TestReplicationBenchmarking(unittest.TestCase):
         # Get inserts sentences
         with open('100_insert.sql') as f:
             contents = f.read()
-    
+   
         # Execute SQL bundle
         cur_master.execute(
             """
             %s
             """ % (contents)
         )
-        cur_master.fetchone()
         conn_master.commit()
 
         # Wait until sync sequence start posting data on the ended column
@@ -64,20 +69,22 @@ class TestReplicationBenchmarking(unittest.TestCase):
                 SELECT * FROM syncrun WHERE ended IS NULL;
                 """
             )
-            result = cur_master.fetchone()
-            if result is not None:
-                duration = timeout
+            result = cur_bucardo.fetchone()
+            if result is None :
+                duration = (datetime.datetime.now() - dt).total_seconds()
+
+            else:
+                duration = timeout + 1
 
             time.sleep(1)
-            duration = (datetime.datetime.now() - dt).total_seconds()
         
         # Get results of the sync delays
         cur_bucardo.execute(
                 """
-                SELECT started, ended, inserted FROM syncrun WHERE ended IS NOT NULL and inserts == %i;
+                SELECT started, ended, inserts FROM syncrun WHERE ended IS NOT NULL and inserts = %i;
                 """ % (id_sync)
             )    
-        result = cur_master.fetchone()
+        result = cur_bucardo.fetchone()
         started, ended, inserted  = result[0], result[1], result[2]
 
         # Results will go to a CSV file
