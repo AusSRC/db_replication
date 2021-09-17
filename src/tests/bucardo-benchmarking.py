8f7b857c15b5cc9623426840badfe60e97dbf3f9
Manuel Jesús Parra Royón <manuparra@gmail.com>
2021-09-17 13:54:25 +0200

    Updated size of the test batch

--- src/tests/bucardo-benchmarking.py
+++ src/tests/bucardo-benchmarking.py
@@ -294,3 +294,392 @@ class TestReplicationBenchmarking(unittest.TestCase):
         conn_bucardo.close()
 
         self.assertTrue(True)
+
+
+    def test_E_benchmark_5000_insert(self):
+        """
+        """
+
+        id_sync = 5000
+        operation = "insert"
+
+        duration = 0.0
+        timeout = 1200.0
+
+        # connect to master and bucardo db
+        conn_master = psycopg2.connect(**self.dbr.master[0])
+        cur_master = conn_master.cursor()
+        conn_bucardo = psycopg2.connect(**self.dbr.bucardo[0])
+        cur_bucardo = conn_bucardo.cursor()
+
+        # Build batch sentences
+        self.bu.buildBatch(nrows=id_sync, table = "wallaby.run", operation = "insert")
+    
+        # Get batch sentences
+        content = self.bu.getBatch(nrows=id_sync, table = "wallaby.run", operation = "insert")    
+   
+        # Execute SQL bundle
+        cur_master.execute(
+            """
+            %s
+            """ % (content)
+        )
+        conn_master.commit()
+
+        # Wait until sync sequence start posting data on the ended column
+        dt = datetime.datetime.now()
+        while duration < timeout:
+            # Execute query on Bucardo DB.
+            cur_bucardo.execute(
+                """
+                SELECT * FROM syncrun WHERE ended IS NULL;
+                """
+            )
+            result = cur_bucardo.fetchone()
+            if result is None :
+                duration = (datetime.datetime.now() - dt).total_seconds()
+
+            else:
+                duration = timeout + 1
+
+            # Wait 1 second to avoid an overflow sending queries
+            time.sleep(3)
+        
+        # Get results of the sync delays
+        cur_bucardo.execute(
+                "SELECT started, ended, inserts FROM syncrun WHERE ended IS NOT NULL order by started DESC;"
+            )    
+        result = cur_bucardo.fetchone()
+        
+        self.bu.addStats(row=result, nrows=id_sync, operation=operation)
+        
+        cur_master.close()
+        conn_master.close()
+        cur_bucardo.close()
+        conn_bucardo.close()
+
+        self.assertTrue(True)
+
+
+    def test_F_benchmark_5000_delete(self):
+        """
+        """
+
+        time.sleep(3)
+
+        id_sync = 5000
+        operation = "delete"
+
+        duration = 0.0
+        timeout = 1200.0
+
+        # connect to master and bucardo db
+        conn_master = psycopg2.connect(**self.dbr.master[0])
+        cur_master = conn_master.cursor()
+        conn_bucardo = psycopg2.connect(**self.dbr.bucardo[0])
+        cur_bucardo = conn_bucardo.cursor()
+
+        # Build batch sentences
+        self.bu.buildBatch(nrows=id_sync, table = "wallaby.run", operation = "delete")
+    
+        # Get batch sentences
+        content = self.bu.getBatch(nrows=id_sync, table = "wallaby.run", operation = "delete")    
+   
+        # Execute SQL bundle
+        cur_master.execute(
+            """
+            %s
+            """ % (content)
+        )
+        conn_master.commit()
+
+        # Wait until sync sequence start posting data on the ended column
+        dt = datetime.datetime.now()
+        while duration < timeout:
+            # Execute query on Bucardo DB.
+            cur_bucardo.execute(
+                """
+                SELECT * FROM syncrun WHERE ended IS NULL;
+                """
+            )
+            result = cur_bucardo.fetchone()
+            if result is None :
+                duration = (datetime.datetime.now() - dt).total_seconds()
+
+            else:
+                duration = timeout + 1
+
+            # Wait 1 second to avoid an overflow sending queries
+            time.sleep(3)
+        
+        # Get results of the sync delays
+        cur_bucardo.execute("SELECT started, ended, deletes FROM syncrun WHERE ended IS NOT NULL order by started DESC;")    
+        result = cur_bucardo.fetchone()
+        
+        self.bu.addStats(row=result, nrows=id_sync, operation=operation)
+        
+        cur_master.close()
+        conn_master.close()
+        cur_bucardo.close()
+        conn_bucardo.close()
+
+        self.assertTrue(True)
+
+
+    def test_G_benchmark_10000_insert(self):
+        """
+        """
+
+        id_sync = 10000
+        operation = "insert"
+
+        duration = 0.0
+        timeout = 1200.0
+
+        # connect to master and bucardo db
+        conn_master = psycopg2.connect(**self.dbr.master[0])
+        cur_master = conn_master.cursor()
+        conn_bucardo = psycopg2.connect(**self.dbr.bucardo[0])
+        cur_bucardo = conn_bucardo.cursor()
+
+        # Build batch sentences
+        self.bu.buildBatch(nrows=id_sync, table = "wallaby.run", operation = "insert")
+    
+        # Get batch sentences
+        content = self.bu.getBatch(nrows=id_sync, table = "wallaby.run", operation = "insert")    
+   
+        # Execute SQL bundle
+        cur_master.execute(
+            """
+            %s
+            """ % (content)
+        )
+        conn_master.commit()
+
+        # Wait until sync sequence start posting data on the ended column
+        dt = datetime.datetime.now()
+        while duration < timeout:
+            # Execute query on Bucardo DB.
+            cur_bucardo.execute(
+                """
+                SELECT * FROM syncrun WHERE ended IS NULL;
+                """
+            )
+            result = cur_bucardo.fetchone()
+            if result is None :
+                duration = (datetime.datetime.now() - dt).total_seconds()
+
+            else:
+                duration = timeout + 1
+
+            # Wait 1 second to avoid an overflow sending queries
+            time.sleep(3)
+        
+        # Get results of the sync delays
+        cur_bucardo.execute(
+                "SELECT started, ended, inserts FROM syncrun WHERE ended IS NOT NULL order by started DESC;"
+            )    
+        result = cur_bucardo.fetchone()
+        
+        self.bu.addStats(row=result, nrows=id_sync, operation=operation)
+        
+        cur_master.close()
+        conn_master.close()
+        cur_bucardo.close()
+        conn_bucardo.close()
+
+        self.assertTrue(True)
+
+
+    def test_H_benchmark_10000_delete(self):
+        """
+        """
+
+        time.sleep(3)
+
+        id_sync = 10000
+        operation = "delete"
+
+        duration = 0.0
+        timeout = 1200.0
+
+        # connect to master and bucardo db
+        conn_master = psycopg2.connect(**self.dbr.master[0])
+        cur_master = conn_master.cursor()
+        conn_bucardo = psycopg2.connect(**self.dbr.bucardo[0])
+        cur_bucardo = conn_bucardo.cursor()
+
+        # Build batch sentences
+        self.bu.buildBatch(nrows=id_sync, table = "wallaby.run", operation = "delete")
+    
+        # Get batch sentences
+        content = self.bu.getBatch(nrows=id_sync, table = "wallaby.run", operation = "delete")    
+   
+        # Execute SQL bundle
+        cur_master.execute(
+            """
+            %s
+            """ % (content)
+        )
+        conn_master.commit()
+
+        # Wait until sync sequence start posting data on the ended column
+        dt = datetime.datetime.now()
+        while duration < timeout:
+            # Execute query on Bucardo DB.
+            cur_bucardo.execute(
+                """
+                SELECT * FROM syncrun WHERE ended IS NULL;
+                """
+            )
+            result = cur_bucardo.fetchone()
+            if result is None :
+                duration = (datetime.datetime.now() - dt).total_seconds()
+
+            else:
+                duration = timeout + 1
+
+            # Wait 1 second to avoid an overflow sending queries
+            time.sleep(3)
+        
+        # Get results of the sync delays
+        cur_bucardo.execute("SELECT started, ended, deletes FROM syncrun WHERE ended IS NOT NULL order by started DESC;")    
+        result = cur_bucardo.fetchone()
+        
+        self.bu.addStats(row=result, nrows=id_sync, operation=operation)
+        
+        cur_master.close()
+        conn_master.close()
+        cur_bucardo.close()
+        conn_bucardo.close()
+
+        self.assertTrue(True)
+
+    def test_I_benchmark_20000_insert(self):
+        """
+        """
+
+        id_sync = 20000
+        operation = "insert"
+
+        duration = 0.0
+        timeout = 1200.0
+
+        # connect to master and bucardo db
+        conn_master = psycopg2.connect(**self.dbr.master[0])
+        cur_master = conn_master.cursor()
+        conn_bucardo = psycopg2.connect(**self.dbr.bucardo[0])
+        cur_bucardo = conn_bucardo.cursor()
+
+        # Build batch sentences
+        self.bu.buildBatch(nrows=id_sync, table = "wallaby.run", operation = "insert")
+    
+        # Get batch sentences
+        content = self.bu.getBatch(nrows=id_sync, table = "wallaby.run", operation = "insert")    
+   
+        # Execute SQL bundle
+        cur_master.execute(
+            """
+            %s
+            """ % (content)
+        )
+        conn_master.commit()
+
+        # Wait until sync sequence start posting data on the ended column
+        dt = datetime.datetime.now()
+        while duration < timeout:
+            # Execute query on Bucardo DB.
+            cur_bucardo.execute(
+                """
+                SELECT * FROM syncrun WHERE ended IS NULL;
+                """
+            )
+            result = cur_bucardo.fetchone()
+            if result is None :
+                duration = (datetime.datetime.now() - dt).total_seconds()
+
+            else:
+                duration = timeout + 1
+
+            # Wait 1 second to avoid an overflow sending queries
+            time.sleep(3)
+        
+        # Get results of the sync delays
+        cur_bucardo.execute(
+                "SELECT started, ended, inserts FROM syncrun WHERE ended IS NOT NULL order by started DESC;"
+            )    
+        result = cur_bucardo.fetchone()
+        
+        self.bu.addStats(row=result, nrows=id_sync, operation=operation)
+        
+        cur_master.close()
+        conn_master.close()
+        cur_bucardo.close()
+        conn_bucardo.close()
+
+        self.assertTrue(True)
+
+
+    def test_J_benchmark_20000_delete(self):
+        """
+        """
+
+        time.sleep(3)
+
+        id_sync = 20000
+        operation = "delete"
+
+        duration = 0.0
+        timeout = 1200.0
+
+        # connect to master and bucardo db
+        conn_master = psycopg2.connect(**self.dbr.master[0])
+        cur_master = conn_master.cursor()
+        conn_bucardo = psycopg2.connect(**self.dbr.bucardo[0])
+        cur_bucardo = conn_bucardo.cursor()
+
+        # Build batch sentences
+        self.bu.buildBatch(nrows=id_sync, table = "wallaby.run", operation = "delete")
+    
+        # Get batch sentences
+        content = self.bu.getBatch(nrows=id_sync, table = "wallaby.run", operation = "delete")    
+   
+        # Execute SQL bundle
+        cur_master.execute(
+            """
+            %s
+            """ % (content)
+        )
+        conn_master.commit()
+
+        # Wait until sync sequence start posting data on the ended column
+        dt = datetime.datetime.now()
+        while duration < timeout:
+            # Execute query on Bucardo DB.
+            cur_bucardo.execute(
+                """
+                SELECT * FROM syncrun WHERE ended IS NULL;
+                """
+            )
+            result = cur_bucardo.fetchone()
+            if result is None :
+                duration = (datetime.datetime.now() - dt).total_seconds()
+
+            else:
+                duration = timeout + 1
+
+            # Wait 1 second to avoid an overflow sending queries
+            time.sleep(3)
+        
+        # Get results of the sync delays
+        cur_bucardo.execute("SELECT started, ended, deletes FROM syncrun WHERE ended IS NOT NULL order by started DESC;")    
+        result = cur_bucardo.fetchone()
+        
+        self.bu.addStats(row=result, nrows=id_sync, operation=operation)
+        
+        cur_master.close()
+        conn_master.close()
+        cur_bucardo.close()
+        conn_bucardo.close()
+
+        self.assertTrue(True)
